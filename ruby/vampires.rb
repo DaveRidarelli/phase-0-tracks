25e7c371fa3f254c25dc46d03190796fbd9f9abb
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-09-17 08:41:04 -0500

    Completed release 5 challenge 4.4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees would you like to test today?"
+number_of_employees = gets.chomp.to_i
+counter = 0
+
+until counter == number_of_employees
 
 	puts "What is your name?"
 	name = gets.chomp
@@ -14,13 +19,13 @@
 	puts "Would you like to enroll in the company’s health insurance?"
 	health_ins = gets.chomp
 
-	if (age + year_born == 2015 || age + year_born == 2016)
-		age_test = true
-	else
+	if (age + year_born < 2015 || age + year_born > 2016)
 		age_test = false
+	else
+		age_test = true
 	end
 
-	if name == "Drake Cula" || name == "Tu Fang" || name == "Dr. Acula"
+	if name = "Drake Cula" || name = "Tu Fang" || name = "Dr. Acula"
 		vamp_test = true
 	else
 		vamp_test = false
@@ -40,6 +45,30 @@
 	else 
 		vampire_status = "Results inconclusive. Bring holy water just in case." 
 	end
-	
 
-puts vampire_status
+	puts "Please type any allergies you have and type \"done\" when complete"
+		allergy = gets.chomp
+
+		if allergy == "sunshine"
+			vampire_status = "Probably a darn vampire"
+		else
+			while allergy != "done"
+				allergy = gets.chomp
+			
+				if allergy == "sunshine"
+					vampire_status = "Probably a darn vampire"
+					allergy = "done"
+				end
+			end
+		end
+	
+	puts vampire_status
+	puts ""
+	counter += 1
+
+	if counter < number_of_employees
+		puts "Employee ##{counter + 1}"
+	else
+		puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends!!!"
+	end
+end
