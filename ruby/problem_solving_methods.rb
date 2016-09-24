ac9fe20cace843f72ca022fb48768246e3294b12
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-09-23 22:02:35 -0500

    Completion of challenge 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,8 +1,10 @@
+
 # Method with 2 arguments, array and integer
 # find length of array
 # initialize a variable
 # loop incrementing variable while using it to reference
 # array and check
+
 def search_array(list, number)
   count=0
   while count<=list.length
@@ -16,9 +18,11 @@ end
 arr = [42, 89, 23, 1]
 p search_array(arr, 1)
 p search_array(arr, 24)
+
 # Initialize with 0 and 1
 # account for fib(1 and 2)
 # count initialized at 2 if it doesn't match above case
+
 def fib(number)
   number=number.to_i
   sequence=[]
@@ -40,3 +44,50 @@ def fib(number)
 end
 fib(2)
 fib(100)
+
+# Pass in an array to our method
+# Filter through array index by index number with incrementation variable
+# Compare current index with following index > , <
+# After comparison switch values if following index is greater
+# Continue through entire array
+# Loop filter method back to first index
+# repeat bubble sort as long as unsorted
+# if no switches made bubble sort complete 
+
+array_to_sort = [ 35 , 14, 98, 77, 2, 55, 63, 22, 31, 64, 5 ]
+
+def bubble_sort(array)
+  counter = 0
+  anti_counter = array.length - 1
+
+  while anti_counter > 1
+
+    # While array is unsorted
+    while counter < anti_counter
+
+    # While loop for comparison
+      if array[counter] > array[counter + 1]
+        number1 = array[counter]
+        number2 = array[counter + 1]
+    # can now assign new positions
+        array[counter] = number2
+        array[counter + 1] = number1
+        counter += 1
+      else
+        counter += 1
+      end 
+    end
+    anti_counter -= 1
+    counter = 0
+  end
+  p array
+end
+
+bubble_sort(array_to_sort)
+
+
+  
+
+
+
+
