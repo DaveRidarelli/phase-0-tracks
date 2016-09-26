c4993577acfb31899779b9d2a2c388bc340542b1
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-09-25 23:16:17 -0500

    Intial Commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# swap first and last names with split( ' ' ) mind the spacing! and .reverse
+# create array of vowels. use .next perhaps to select next vowel in array ala encrypt decrypt challenge
+# q's
+# intially and with the letters
+# create an array
+# .downcase!! then upcase
+# find them as they come
+# break methods down!
+# interface should be ez
+# array for names INDEX
