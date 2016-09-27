9101b26f470a6894b9e43bc3c4d28b97973f9a89
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-09-27 03:52:38 -0500

    Initial commit for puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Super Cute Puppy Pups
+
+# CHARACTERISTICS ----------------
+# Furry: yes
+# Cute puppy face: yes
+# Poops alot: most likely
+# Name : possibly sir barks alot
+# Chew toys required: 15
+
+# BEHAVIOR -----------------------
+# Pant
+# Chase Squirrels
+# Bark erratically
+
