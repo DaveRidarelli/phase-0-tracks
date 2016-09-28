55a2c3aa785b3226c7b48a12c8908c9d2fe860df
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-09-27 21:44:41 -0500

    Completed challenge 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,39 +1,40 @@
-# class Puppy
+class Puppy
 
-#   def fetch(toy)
-#     puts "I brought back the #{toy}!"
-#     toy
-#   end
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
 
-#   def speak(number_of_times)
-#   	puts "Woof! " * number_of_times
-#   end
+  def speak(number_of_times)
+  	puts "Woof! " * number_of_times
+  end
 
-#   def roll_over
-#   	puts "rolls over"
-#   end
+  def roll_over
+  	puts "rolls over"
+  end
 
-#   def dog_years(human_years)
-#   		puts (human_years / 7).to_s
-#   end
+  def dog_years(human_years)
+  		puts (human_years / 7).to_s
+  end
 
-#   def human_years(dog_years)
-#   		puts (dog_years * 7).to_s
-#   end
+  def human_years(dog_years)
+  		puts (dog_years * 7).to_s
+  end
 
-#   def initialize
-#   		puts "Initialzing new puppy instance ..."
-#   end
+  def initialize
+  		puts "Initialzing new puppy instance ..."
+  end
 
-# end
+end
 
-# new_puppy = Puppy.new
-# new_puppy.fetch("ball")
-# new_puppy.speak(5)
-# new_puppy.roll_over
-# new_puppy.dog_years(55)
-# new_puppy.human_years(7)
+new_puppy = Puppy.new
+new_puppy.fetch("ball")
+new_puppy.speak(5)
+new_puppy.roll_over
+new_puppy.dog_years(55)
+new_puppy.human_years(7)
 
+#METHOD CODE
 class Kitty_cats
 
 	def initialize
@@ -49,6 +50,30 @@ class Kitty_cats
 	end
 
 end
+
+#DRIVER CODE
+
+mr_paws = Kitty_cats.new
+
+mr_paws.scratch("couch")
+
+mr_paws.sleep(15)
+
+kitty_array = []
+
+50.times { kitty_array << Kitty_cats.new }
+
+# puts kitty_array # DEBUG
+
+kitty_array.each do |x| 
+
+	x.scratch("kitty post") 
+	
+	x.sleep(22)
+	
+end
+
+ 
 
 
 
