bad08519771a7d0e4c4bbe80c95008a575b8697d
Dave Ridarelli <DavidRidarelli@gmail.com>
2016-10-02 21:10:00 -0500

    Completed Challenge 6.4 Modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,36 @@
+# module Shout
+#   def self.yell_angrily(words)
+#   	words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words + "!!!!!" + " =) "
+#   end
+# end
+
+# #Driver Code
+# puts Shout.yell_angrily("I don't know what we're yelling about")
+# puts Shout.yelling_happily("Hurray for Science")
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def yell_angrily(words)
+  	words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+  	words + "!!!!!" + " =) "
+  end
 end
+
+class CrankyOldMan
+	include Shout
+end
+
+class HappyGraduate
+	include Shout
+end
+
+old_man = CrankyOldMan.new
+puts old_man.yell_angrily("Get off my property you damn kids")
+happy_grad = HappyGraduate.new
+puts happy_grad.yelling_happily("I'm the king of the world")
